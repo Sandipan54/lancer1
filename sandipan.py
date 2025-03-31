3e39c70d061e422b35486f7e2ebe4b72db060c40
Sandipan54 <130487031+Sandipan54@users.noreply.github.com>
2025-03-31 18:44:34 +0530

    sandipan.py

--- sandipan.py
+++ sandipan.py
@@ -1,17 +1,11 @@
-# Don't use quotes( " and ' )
-#SCRIPT BY @LancerPlaysOP
-  
-#Enter Your Bot Token here get it from @botfather
+# Don't use quotes(  and  )
+# SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+
+# Enter Your Bot Token here get it from @botfarher
 BOT_TOKEN=("7656392842:AAE5hi-EHjgIlzw3PnBlwuQxBjkZLJcJdH8")
 
-  #Enter Your telegram username here without @
+# Enter Your telegram usernames here without @
 OWNER_USERNAME=("@Sandipan_1508")
 
-  #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6228283651")
-
-
-
-
-
-  
+# Enter your admin IDs here. Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6228283651", "859818729")
