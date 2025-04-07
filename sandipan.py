7a97ccb21920e3160bb65a442aa04a12287d026a
Sandipan54 <130487031+Sandipan54@users.noreply.github.com>
2025-04-07 12:15:42 +0530

    Add files via upload

---
+++ sandipan.py
@@ -0,0 +1,11 @@
+# Don't use quotes(  and  )
+# SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+
+# Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7769529248:AAF8Ct-mt2k2j1MC1ZaJFOS7_d3I9OGjKYg")
+
+# Enter Your telegram usernames here without @
+OWNER_USERNAME=("@Sandipan_1508")
+
+# Enter your admin IDs here. Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6228283651", "859818729")
