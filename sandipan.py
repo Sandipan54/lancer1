b302c44a778c34b1ba374286fefb277d0a1cd88d
Sandipan54 <130487031+Sandipan54@users.noreply.github.com>
2025-04-08 16:39:59 +0530

    sandipan.py

--- sandipan.py
+++ sandipan.py
@@ -2,10 +2,10 @@
 # SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
 
 # Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7769529248:AAF8Ct-mt2k2j1MC1ZaJFOS7_d3I9OGjKYg")
+BOT_TOKEN=("7892208728:AAEUUeci_pJ_yuRYZf2pz8WIh9br4tYm7as")
 
 # Enter Your telegram usernames here without @
 OWNER_USERNAME=("@Sandipan_1508")
 
 # Enter your admin IDs here. Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6228283651", "859818729")
+ADMIN_IDS=("6228283651", "5577479397")
